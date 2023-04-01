0c70f8d329a2443ec0bf18dadbb7e110c1aceff3
Yuliia Karas <iuliiakaras@gmail.com>
2023-04-01 18:52:48 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+console.log(galleryList);
+
+const galleryMarkup = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+};
+
+const createGallery = galleryItems.reduce((acc, item) => {
+  return acc + galleryMarkup(item);
+}, '');
+
+galleryList.insertAdjacentHTML('beforeend', createGallery);
+galleryList.addEventListener('click', modalOpenLightBox);
+
+function modalOpenLightBox(event) {
+  event.preventDefault();
+  const img = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${img}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryList.addEventListener('keydown', escClick);
+  function escClick(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
