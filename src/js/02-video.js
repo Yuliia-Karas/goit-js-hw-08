c2d65b97b5b7854f901d2625fa5211dea1cd6b48
Yuliia Karas <iuliiakaras@gmail.com>
2023-04-04 20:45:58 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
 
+function saveCurrentTime() {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem('videoplayer-current-time', Math.round(time));
+  });
+}
 
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const iframe = document.querySelector('iframe');
-
-// const player = new Player(iframe);
-// const getCurrentTime = localStorage.getItem('videoplayer-current-time');
-
-// if (getCurrentTime) {
-//   player.setCurrentTime(getCurrentTime);
-// }
-
-// player.on('timeupdate', throttle(currentTime, 1000));
-
-// function currentTime(data) {
-//   let currentTime = data.seconds;
-//   localStorage.setItem('videoplayer-current-time', currentTime);
-// }
-
-// player.on(
-//   'timeupdate',
-//   throttle(evt => {
-//     localStorage.setItem('videoplayer-current-time', evt.seconds);
-//   }, 1000)
-// );
-
-// player
-//   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-//   .catch(err => {
-//     console.error(err);
-//   });
+window.addEventListener('load', () => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,3 @@
-
-
-
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const allData = {};
