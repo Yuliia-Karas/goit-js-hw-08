f2d2d10bde1a58dcb26feab0dc705528665d97f0
Yuliia Karas <iuliiakaras@gmail.com>
2023-04-05 21:50:05 +0200

    change constants

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,23 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const videoPlayerCurrentTime = 'videoplayer-current-time';
-const timeUpdate = 1000;
+const VIDEO_PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const TIME_UPDATE = 1000;
 
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-player.on('timeupdate', throttle(saveCurrentTime, timeUpdate));
+player.on('timeupdate', throttle(saveCurrentTime, TIME_UPDATE));
 
 function saveCurrentTime() {
     player.getCurrentTime().then(time => {
         
-    localStorage.setItem(videoPlayerCurrentTime, Math.round(time));
+    localStorage.setItem(VIDEO_PLAYER_CURRENT_TIME, Math.round(time));
   });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  const currentTime = localStorage.getItem(videoPlayerCurrentTime);
+  const currentTime = localStorage.getItem(VIDEO_PLAYER_CURRENT_TIME);
   if (currentTime) {
     player.setCurrentTime(currentTime);
   }
